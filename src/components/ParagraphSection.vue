9adf82a689fbe92fa13c3b952e7e7c6c849d23e2
Julian Montero <71648137+JulianMGonzalez@users.noreply.github.com>
2021-10-07 23:18:09 -0500

    create paragraph component

---
+++ src/components/ParagraphSection.vue
@@ -0,0 +1,18 @@
+<template>
+  <p class="text-lg nav ">
+    {{description}}
+  </p>
+</template>
+
+<script>
+export default {
+    props:{
+        description: {
+            type: String
+        }
+    }
+};
+</script>
+
+<style>
+</style>
